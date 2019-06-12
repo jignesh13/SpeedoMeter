4c12450c3a4a6b2b143b96e2b132315af45bceb0
macmini1 <jigneshkhunt13@gmail.com>
2019-06-12 17:32:29 +0530

    add library

--- app/src/main/java/com/example/speedometer/MainActivity.java
+++ app/src/main/java/com/example/speedometer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.speedometer;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,10 +8,14 @@ import android.widget.SeekBar;
 
 import com.jignesh13.speedometer.SpeedoMeterView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    private int[] color={R.color.colorAccent,R.color.black,R.color.white,R.color.yellow,R.color.blue,R.color.fuchsia};
     private SpeedoMeterView speedoMeterView;
     private SeekBar seekBar;
+    private Random random=new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void okBtnClick(View view) {
         speedoMeterView.setSpeed(seekBar.getProgress(),true);
+    }
+
+    public void removeborder(View view) {
+        speedoMeterView.setisborder(!speedoMeterView.isborder());
+    }
+
+    public void linecolorchange(View view) {
+        int color= Color.rgb(random.nextInt(255),random.nextInt(255),random.nextInt(255));
+        speedoMeterView.setLinecolor(color);
+    }
+
+    public void needlecolorchange(View view) {
+        int color= Color.rgb(random.nextInt(255),random.nextInt(255),random.nextInt(255));
+        speedoMeterView.setNeedlecolor(color);
+    }
+
+    public void backColorChange(View view) {
+
+        speedoMeterView.setbackImageResource(color[random.nextInt(color.length)]);
     }
 }
--- speedometer/src/main/java/com/jignesh13/speedometer/SpeedoMeterView.java
+++ speedometer/src/main/java/com/jignesh13/speedometer/SpeedoMeterView.java
@@ -4,7 +4,9 @@ import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
@@ -12,7 +14,12 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.ContextCompat;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
@@ -24,7 +31,7 @@ import android.view.animation.DecelerateInterpolator;
 import java.util.Random;
 
 public class SpeedoMeterView extends View {
-    private Bitmap backgroundbitmap;
+    private Bitmap backgroundbitmap,backimagebitmap;
     private float centerx,centery,radius;
     private Path path;
     private boolean isSpeedIncrease = false;
@@ -34,7 +41,15 @@ public class SpeedoMeterView extends View {
     private Animator.AnimatorListener animatorListener;
     private float trembleDegree = 4f;
     private boolean canceled = false;
+    private Drawable drawable;
+    private boolean isborder;
+    private int linecolor,needlecolor;
     private Paint paint=new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public boolean isborder() {
+        return isborder;
+    }
+
     public SpeedoMeterView(Context context) {
         this(context,null);
     }
@@ -45,8 +60,44 @@ public class SpeedoMeterView extends View {
 
     public SpeedoMeterView(Context context,  AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        if(attrs == null){
+            return;
+        }
+
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.MyCustomView);
+        needlecolor=ta.getColor(R.styleable.MyCustomView_needlecolor,Color.WHITE);
+        linecolor=ta.getColor(R.styleable.MyCustomView_linecolor,Color.WHITE);
+        drawable = ta.getDrawable(R.styleable.MyCustomView_backimage);
+        isborder=ta.getBoolean(R.styleable.MyCustomView_removeborder,true);
+
         init();
 
+    }
+    public Bitmap transform(Bitmap source) {
+        source=Bitmap.createScaledBitmap(source,getWidth(),getHeight(),false);
+        int size = Math.min(source.getWidth(),source.getHeight());
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+
+        Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+        if (squaredBitmap != source) {
+            source.recycle();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        BitmapShader shader = new BitmapShader(squaredBitmap,
+                BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+        paint.setShader(shader);
+        paint.setAntiAlias(true);
+
+
+        canvas.drawCircle(centerx, centery, radius-((radius/9.85f)*2.0f), paint);
+
+        squaredBitmap.recycle();
+        return bitmap;
     }
     private void init(){
         speedAnimator = ValueAnimator.ofFloat(0f, 1f);
@@ -102,77 +153,122 @@ public class SpeedoMeterView extends View {
         trembleAnimator.addListener(animatorListener);
         trembleAnimator.start();
     }
+    public void setbackImageResource(@DrawableRes int resId){
+        drawable = ContextCompat.getDrawable(getContext(),resId);
+        createback();
+        invalidate();
 
+    }
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         if (getWidth()>0&&getHeight()>0){
-            centerx=getWidth()/2.0f;
-            centery=getHeight()/2.0f;
-            radius=Math.min(getWidth()/2.0f,getHeight()/2.0f);
-            createSpeedometerDisk();
-            path=new Path();
-            float  bottomY = getWidth()/1.6f ;
-            path.moveTo(getWidth()/2.0f,80);
-            path.lineTo(centerx-5,80);
-            path.lineTo(centerx-10,bottomY);
-            path.lineTo(centerx+10,bottomY);
-            path.lineTo(centerx+5,80);
+           createback();
         }
     }
+    public void createback(){
+        centerx=getWidth()/2.0f;
+        centery=getHeight()/2.0f;
 
+        radius=Math.min(getWidth()/2.0f,getHeight()/2.0f);
+        if (drawable!=null){
+            backimagebitmap=transform(drawableToBitmap(drawable));
+        }
+        createSpeedometerDisk();
+        path=new Path();
+        float  bottomY = getWidth()/1.6f ;
+        path.moveTo(getWidth()/2.0f,radius/4.0f);
+        path.lineTo(centerx-(radius/78.8f),radius/4.0f);
+        path.lineTo(centerx-(radius/39.4f),bottomY);
+        path.lineTo(centerx+(radius/39.4f),bottomY);
+        path.lineTo(centerx+(radius/78.8f),radius/4.0f);
+    }
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        paint.setColor(Color.BLACK);
+        if (backimagebitmap!=null)canvas.drawBitmap(backimagebitmap,0,0,paint);
         if (backgroundbitmap!=null){
             paint.setColor(Color.BLACK);
             canvas.drawBitmap(backgroundbitmap,0,0,paint);
         }
 
-        float angle=215.0f+(252*currentSpeed)/140.0f;
 
-        paint.setStrokeWidth(6);
-        paint.setStyle(Paint.Style.STROKE);
-        Log.e("Angle",angle+"");
-        if (currentSpeed>100){
-            paint.setColor(Color.RED);
-            canvas.drawArc(new RectF(80,80,getWidth()-80,getHeight()-80),125,angle-215,false,paint);
-            paint.setColor(Color.GREEN);
-            canvas.drawArc(new RectF(80,80,getWidth()-80,getHeight()-80),125,180,false,paint);
-        }
-        else {
-            paint.setColor(Color.GREEN);
-            canvas.drawArc(new RectF(80,80,getWidth()-80,getHeight()-80),125,angle-215,false,paint);
-        }
+        float angle=215.0f+(252*currentSpeed)/140.0f;
         canvas.save();
         canvas.rotate(angle,centerx,centery);
         paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.rgb(208,50,0));
+        paint.setColor(needlecolor);
         paint.setStyle(Paint.Style.FILL);
         canvas.drawPath(path,paint);
-        canvas.drawCircle(centerx,centery,20,paint);
-        paint.setColor(Color.BLACK);
-        canvas.drawCircle(centerx,centery,2,paint);
+        canvas.drawCircle(centerx,centery,radius/19.7f,paint);
+        paint.setColor(getComplimentColor(needlecolor));
+        canvas.drawCircle(centerx,centery,radius/197.0f,paint);
         canvas.restore();
-
 
     }
 
+    public void setisborder(boolean isborder) {
+        this.isborder = isborder;
+        createSpeedometerDisk();
+        invalidate();
+    }
+
+
+
+    public static int getComplimentColor(int color) {
+        // get existing colors
+        int alpha = Color.alpha(color);
+        int red = Color.red(color);
+        int blue = Color.blue(color);
+        int green = Color.green(color);
+
+        // find compliments
+        red = (~red) & 0xff;
+        blue = (~blue) & 0xff;
+        green = (~green) & 0xff;
+
+        return Color.argb(alpha, red, green, blue);
+    }
+
     private void createSpeedometerDisk() {
+        Log.e("radius",radius+"");
+        float strokewidth=radius/9.85f;
         backgroundbitmap=Bitmap.createBitmap(getWidth(),getHeight(), Bitmap.Config.ARGB_4444);
         Canvas canvas=new Canvas(backgroundbitmap);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(40);
+        paint.setStrokeWidth(strokewidth);
         Shader shader = new LinearGradient(0, 0, getWidth(), getHeight(), Color.rgb(181,181,181), Color.rgb(0,0,0), Shader.TileMode.CLAMP);
         paint.setShader(shader);
-        canvas.drawCircle(centerx,centery,radius-20,paint);
+       if (isborder)canvas.drawCircle(centerx,centery,radius-(strokewidth/2.0f),paint);
         Shader shader1 = new LinearGradient(0, 0, getWidth(), getHeight(), Color.rgb(0,0,0), Color.rgb(181,181,181), Shader.TileMode.CLAMP);
         paint.setShader(shader1);
-        canvas.drawCircle(centerx,centerx,radius-60,paint);
+       if (isborder)canvas.drawCircle(centerx,centerx,radius-(strokewidth+(strokewidth/2.0f)),paint);
         paint.setShader(null);
         paint.setColor(Color.rgb(35,35 ,35));
         paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(centerx,centerx,radius-80,paint);
+        if (backimagebitmap==null)canvas.drawCircle(centerx,centerx,radius-(strokewidth*2.0f),paint);
         paint.setStyle(Paint.Style.STROKE);
 
 //        paint.setColor(Color.RED);
@@ -181,30 +277,38 @@ public class SpeedoMeterView extends View {
 //        paint.setColor(Color.GREEN);
 //        canvas.drawArc(new RectF(80,80,getWidth()-80,getHeight()-80),125,180,false,paint);
 
+
+        paint.setStrokeWidth(radius/26.26f);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.RED);
+        canvas.drawArc(new RectF((strokewidth*2.0f)+paint.getStrokeWidth()/2.0f,(strokewidth*2.0f)+paint.getStrokeWidth()/2.0f,getWidth()-(strokewidth*2.0f)-paint.getStrokeWidth()/2.0f,getHeight()-(strokewidth*2.0f)-paint.getStrokeWidth()/2.0f),125,252,false,paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawArc(new RectF((strokewidth*2.0f)+paint.getStrokeWidth()/2.0f,(strokewidth*2.0f)+paint.getStrokeWidth()/2.0f,getWidth()-(strokewidth*2.0f)-paint.getStrokeWidth()/2.0f,getHeight()-(strokewidth*2.0f)-paint.getStrokeWidth()/2.0f),125,180,false,paint);
+
         Paint linepaint=new Paint(Paint.ANTI_ALIAS_FLAG);
-        linepaint.setColor(Color.WHITE);
+        linepaint.setColor(linecolor);
         int point=-10;
         int lastodd=0;
         for (int i=90+125;i<=90+125+252;i+=9f){
 
             canvas.save();
             canvas.rotate(i,centerx,centery);
-            canvas.translate(0,80);
+            canvas.translate(0,strokewidth*2.0f);
 
             if (lastodd==0){
-                linepaint.setStrokeWidth(6);
-                canvas.drawLine(getWidth()/2.0f,0,getWidth()/2.0f,22,linepaint);
+                linepaint.setStrokeWidth(radius/65.66f);
+                canvas.drawLine(getWidth()/2.0f,0,getWidth()/2.0f,radius/12.90f,linepaint);
             }
             else {
-                linepaint.setStrokeWidth(4);
-                canvas.drawLine(getWidth()/2.0f,0,getWidth()/2.0f,15,linepaint);
+                linepaint.setStrokeWidth(radius/98.5f);
+                canvas.drawLine(getWidth()/2.0f,0,getWidth()/2.0f,radius/24.26f,linepaint);
             }
             if (lastodd==0){
-                canvas.translate(0,30);
+                canvas.translate(0,radius/13.13f);
                 point+=10;
                 TextPaint textPaint=new TextPaint(Paint.ANTI_ALIAS_FLAG);
-                textPaint.setTextSize(30);
-                textPaint.setColor(Color.WHITE);
+                textPaint.setTextSize(radius/13.13f);
+                textPaint.setColor(linecolor);
                 StaticLayout staticLayout=  new StaticLayout(point+"", textPaint, getWidth()
                         , Layout.Alignment.ALIGN_CENTER, 0.0f, 0.0f, false);
                 staticLayout.draw(canvas);
@@ -212,8 +316,19 @@ public class SpeedoMeterView extends View {
             lastodd=lastodd==0?1:0;
             canvas.restore();
         }
-
     }
+
+    public void setNeedlecolor(@ColorInt int needlecolor) {
+        this.needlecolor = needlecolor;
+        invalidate();
+    }
+
+    public void setLinecolor(@ColorInt int linecolor) {
+        this.linecolor = linecolor;
+        createSpeedometerDisk();
+        invalidate();
+    }
+
 
     public void setSpeed(int speed, boolean wantmaintainspeed){
         speed = (speed > 140) ? 140 : (speed < 0) ? 0 : speed;
